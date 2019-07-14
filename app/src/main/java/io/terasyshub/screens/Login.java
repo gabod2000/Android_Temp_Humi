fa655d1c3edbdf98ca14d5b95e33e6785519bd28
manukaprabath <manukaprabath.bk@gmail.com>
2019-07-14 09:40:14 +0530

    initial commit

---
+++ app/src/main/java/io/terasyshub/screens/Login.java
@@ -0,0 +1,15 @@
+package io.terasyshub.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.terasyshub.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
